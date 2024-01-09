7bff207fc54b2c7b6fd03662eac9747e8a63caa2
Darkuse <valleriija@gmail.com>
2024-01-09 08:21:12 +0200

    Updated OnVisabilityChange
    Now OnVisibalityChange sends trigger to unity

---
+++ Assets/Code/CallFromWallpaperService.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using UnityEngine;
+
+// Place this script within the "CallFromWallpaperService" GameObject.
+// Alternatively, in the Wallpaper Service within the onVisibilityChanged method,
+// Make sure to adjust the first parameter of UnitySendMessage to match the GameObject name of this script.
+public class CallFromWallpaperService : MonoBehaviour
+{
+    public CanvasGroup mainCanvasGroup;
+
+    private Coroutine coroutineOnFadeOut;
+    private Coroutine coroutineOnFadeIn;
+
+    // Invoked by the Wallpaper Service upon visibility changes.
+    // Receives a boolean value indicating the user's current screen.
+    public void OnVisibilityChange(string boolean)
+    {
+        // If the condition is true, it means the user is in a desktop; if false, the user is in an application.
+        if (boolean == "true")
+        {
+            if (coroutineOnFadeIn != null)
+                StopCoroutine(coroutineOnFadeIn);
+            coroutineOnFadeOut = StartCoroutine(DoFadeOut());
+        }
+        else
+        {
+            if (coroutineOnFadeOut != null)
+                StopCoroutine(coroutineOnFadeOut);
+            mainCanvasGroup.gameObject.SetActive(true);
+            mainCanvasGroup.alpha = 0;
+            coroutineOnFadeIn = StartCoroutine(DoFadeIn());
+        }
+    }
+
+    private IEnumerator DoFadeIn()
+    {
+        float duration = 0.6f;
+        float currentTime = 0f;
+        while (currentTime < duration)
+        {
+            float alpha = Mathf.Lerp(0f, 1f, currentTime / duration);
+            mainCanvasGroup.alpha = alpha;
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+        mainCanvasGroup.alpha = 1f;
+    }
+
+    private IEnumerator DoFadeOut()
+    {
+        float duration = 0.6f;
+        float currentTime = 0f;
+        while (currentTime < duration)
+        {
+            float alpha = Mathf.Lerp(1f, 0f, currentTime / duration);
+            mainCanvasGroup.alpha = alpha;
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+        mainCanvasGroup.alpha = 0f;
+        mainCanvasGroup.gameObject.SetActive(false);
+    }
+}
