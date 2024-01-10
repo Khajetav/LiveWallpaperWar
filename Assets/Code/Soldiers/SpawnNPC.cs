f72a21788301cd2d8c09d5f181ce44421d82d3bb
Khajetav <khajetav@gmail.com>
2024-01-10 21:53:08 +0200

    Soldier adjustments

--- Assets/Code/Soldiers/SpawnNPC.cs
+++ Assets/Code/Soldiers/SpawnNPC.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 
 public class SpawnNPC : MonoBehaviour
 {
-    int rotation = 0;
+    int pathIndex = 0;
     public Transform npcSpawnPoint;
     public GameObject soldierPrefab;
     public List<GameObject> parentNodeList = new List<GameObject>();
 
     void Start()
     {
-        InvokeRepeating("ReleaseNPC", 0, 60);
+        InvokeRepeating("ReleaseNPC", 0, 15);
     }
 
     void ReleaseNPC()
     {
-        GameObject newGameObject = Instantiate(soldierPrefab);
-        newGameObject.GetComponent<GetAllMovementNodes>().AssignNodeParent(parentNodeList[rotation]);
-        newGameObject.transform.position = parentNodeList[rotation].transform.position;
 
-        rotation++;
-        if (parentNodeList.Count <= rotation)
-            rotation = 0;
+        GameObject newSoldierObject = Instantiate(soldierPrefab);
+        newSoldierObject.GetComponent<GetAllMovementNodes>().AssignNodeParent(parentNodeList[pathIndex]);
+        newSoldierObject.GetComponent<GetAllMovementNodes>().LoadAllNodes();
+        GameObject firstMovementNode = newSoldierObject.GetComponent<GetAllMovementNodes>().movementNodes[0];
+        // Set the position
+        Vector3 newPosition = new Vector3(firstMovementNode.transform.position.x, firstMovementNode.transform.position.y, -0.01f);
+        newSoldierObject.transform.position = newPosition;
+
+        pathIndex++;
+        if (parentNodeList.Count <= pathIndex)
+            pathIndex = 0;
     }
 }
