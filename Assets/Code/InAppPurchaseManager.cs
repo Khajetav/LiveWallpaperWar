f2c655b6ecd4a67412b2a2f52091fb43fc436fd9
Darkuse <valleriija@gmail.com>
2024-01-27 15:22:43 +0200

    Implemented non-test inAppPurchase
    Implemented non-test inAppPurchase for testing in google play market

--- Assets/Code/InAppPurchaseManager.cs
+++ Assets/Code/InAppPurchaseManager.cs
@@ -8,35 +8,38 @@ using UnityEngine.UI;
 
 public class InAppPurchaseManager : MonoBehaviour
 {
-    // Products id
-    private string test = "test";
-    // Image for showing interaction with purchase
-    public Image testImage;
+    // Products id 
+    [Header("premium purchase objects")]
+    private string premiumItem = "premium_item";
+    public CanvasGroup premiumCanvas;
+    public GameObject permiumHolderGameObject;
 
     public string environment = "production";
 
-    //async void Start()
-    //{
-    //    try
-    //    {
-    //        var options = new InitializationOptions()
-    //            .SetEnvironmentName(environment);
+    async void Start()
+    {
+        try
+        {
+            var options = new InitializationOptions()
+                .SetEnvironmentName(environment);
 
-    //        await UnityServices.InitializeAsync(options);
-    //    }
-    //    catch (Exception exception)
-    //    {
-    //        Debug.Log("Initialize error: "+ exception.Message);
-    //    }
-    //}
+            await UnityServices.InitializeAsync(options);
+        }
+        catch (Exception exception)
+        {
+            Debug.Log("Initialize error: " + exception.Message);
+        }
+    }
 
     public void OnPurchaseComplete(Product product)
     {
+        Debug.Log("Purchase completed: "+ product.definition.id);
         // To change test image color for green
-        if (product.definition.id == test)
+        if (product.definition.id == premiumItem)
         {
-            Debug.Log("Test purchase");
-            testImage.color = Color.green;
+            permiumHolderGameObject.GetComponent<Button>().enabled = false;
+            premiumCanvas.interactable = true;
+            premiumCanvas.blocksRaycasts = true;
         }
     }
 
@@ -44,11 +47,6 @@ public class InAppPurchaseManager : MonoBehaviour
     {
         Debug.Log(product.definition.id + " failed. " + failureReason.message);
 
-        if (product.definition.id == test)
-        {
-            Debug.Log("Test purchase");
-            testImage.color = Color.red;
-        }
     }
 
     public void OnTransactionsRestored(bool success, string? error)
@@ -58,9 +56,5 @@ public class InAppPurchaseManager : MonoBehaviour
 
     public void OnProductFetched(Product product)
     {
-        if (product.definition.id == test)
-        {
-            Debug.Log("Test purchase");
-        }
     }
 }
